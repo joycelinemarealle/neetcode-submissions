34469e3765e20021bdeee2ef7113cb4d35911f02
JOYCELINE MAREALLE <149646155+joycelinemarealle@users.noreply.github.com>
2026-05-25 13:20:07 -0400

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        s_dic = {}
+        t_dic = {}
+
+        #firt disqualifier, no need to proceed if this does not work
+        if len(s)!= len(t):
+            return False
+
+        #add counts in dictionary
+        for sval in s:
+            s_dic[sval] = s_dic.get(sval, 0) + 1 
+        
+        for tval in t:
+            t_dic[tval] = t_dic.get(tval, 0) + 1
+        
+        #compare counts of key
+        return s_dic == t_dic
+
+
+
+        
+        #same length, case letters for string to mean the same
+        #check length of two string not same return false
+        #loop hrough s and track counts of each letter
+        #loop through t track counts of each letter
+        #compare counts of each letter if not same return False
+        #return true
+        #Time Big O(m) + Big O(n) worst case loop through all
+        # Memory Big O(1) 
